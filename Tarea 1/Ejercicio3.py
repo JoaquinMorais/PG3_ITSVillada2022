728cabed2acbb2064ed1b25bba71974f63719cba
JoaquinMorais <joaquinmorais@gmail.com>
2022-04-08 09:04:04 -0300

    Triangulo de un caracter arreglado

--- Tarea 1/Ejercicio3.py
+++ Tarea 1/Ejercicio3.py
@@ -1,13 +1,25 @@
+
+
 def pintarCuadrado(caracter,alto,ancho):
     for i in range(alto):
         print(str(caracter)*ancho)
 
 def pintarTriangulo(caracter,alto):
-    largo = len(str(caracter)*alto)
+    
 
+    boolean = False
+    largo = len(str(caracter)*alto)
+    if(len(caracter)%2 != 0):
+        boolean = True
     for i in range(alto):
+
         aux = (str(caracter)*i).center(largo)
+        if(boolean):
+            aux = (str(caracter)*i)
+        else:
+            aux = (str(caracter)*i).center(largo)
         print(aux)
+        
     
 
 
