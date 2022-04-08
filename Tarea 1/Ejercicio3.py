c7eb4089ccc0189fb618536fde6ff545e5bcdc48
JoaquinMorais <joaquinmorais@gmail.com>
2022-04-08 08:45:46 -0300

    triangulo añadido

--- Tarea 1/Ejercicio3.py
+++ Tarea 1/Ejercicio3.py
@@ -1,9 +1,26 @@
-def pintar(caracter,alto,ancho):
+def pintarCuadrado(caracter,alto,ancho):
     for i in range(alto):
         print(str(caracter)*ancho)
-    
-car = input("Ingrese un caracter: ")
-alto = int(input("Ingrese la altura: "))
-ancho = int(input("Ingrese el ancho: "))
 
-pintar(car,alto,ancho)
+def pintarTriangulo(caracter,alto):
+    largo = len(str(caracter)*alto)
+
+    for i in range(alto):
+        aux = (str(caracter)*i).center(largo)
+        print(aux)
+    
+
+
+def main():
+    print("Elige Que Realizar:\n1) Cuadrado \n2) Triangulo")
+    opcion = int(input("Ingrese la opcion: "))
+
+    caracter = input("Ingrese un caracter: ")
+    alto = int(input("Ingrese la altura: "))
+
+    if opcion == 1:
+        ancho = int(input("Ingrese el ancho: "))
+        pintarCuadrado(caracter,alto,ancho)
+    else:
+        pintarTriangulo(caracter,alto)
+main()
