30b0310997fa06be68a80404e5743505f7f55c73
JoaquinMorais <joaquinmorais@gmail.com>
2022-05-06 09:04:10 -0300

    Listo

--- Tarea 3/Ejercicio06.py
+++ Tarea 3/Ejercicio06.py
@@ -1,13 +1,40 @@
 import mysql.connector
 
 
-def main():
+def openUser():
     print("Tratando de meternos a una base de datos...")
     try:
-        conexion = mysql.connector.connect(user='root', password='root', host='localhost', database='Ejercicio01')
+        conexion = mysql.connector.connect(user='bdi', password='Pepe_1234', host='localhost', database='PruebasPython')
         cursor = conexion.cursor()
         print("Conexion exitosa!!!")
-    except:
+        
+        getTable(cursor)
+    except (mysql.connector.errors.ProgrammingError, mysql.connector.errors.DatabaseError) as e:
+        print(e)
         print("No se pudo conectar a la base de datos...")
+        
+
+
+
+def getTable(cursor):
+    tabla = str(input("Ingrese el nombre de la tabla que quieres VER: \n>> "))
+    try:
+        cursor.execute(f"SELECT * FROM {tabla}")
+        for id,nombre,edad in cursor.fetchall():
+            print(id)
+            print(nombre)
+            print(edad)
+        
+    except mysql.connector.errors.ProgrammingError as e:
+        print(e)
+        print("Error al entrar en la tabla...")
+
+
+
+def main():
+    openUser()
+    
+    
 
 main()
+#cursor.close()
